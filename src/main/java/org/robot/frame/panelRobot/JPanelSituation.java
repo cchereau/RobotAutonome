6a0e3dfb1c8c6099cc69ca0eadfa785ac7316eca
cchereau <cchereau@yahoo.fr>
2023-05-07 15:50:12 +0200

    Update JPanelSituation.java

--- src/main/java/org/robot/frame/panelRobot/JPanelSituation.java
+++ src/main/java/org/robot/frame/panelRobot/JPanelSituation.java
@@ -19,14 +19,14 @@ public class JPanelSituation extends JPanel {
         // CONFIGURATION DU PANEL GOOGLE
         /////////////////////////////////////////////////////////////////////////////
         try {
-            panelGoogle.setApiKey("AIzaSyAhNrcx3CrscwK5dbGC7jm_iLGGW28U3zY");
+            panelGoogle.setApiKey("XYZ");
             //panelGoogle.setRoadmap(panelGoogle.viewHybrid);
 
             /**Afficher la ville de Strabourg*/
             // panelGoogle.showLocation("paris", "france", 390, 400);
 
             /** Afficher Paris en fonction ses coordonnées GPS */
-            panelGoogle.showCoordinate("48.9068908691", "2.1749124527", 800, 400);
+            panelGoogle.showCoordinate("X.XXX", "Y.YYY", 800, 400);
         } catch (Exception ex) {
             Logger.getLogger(JEditorPaneAPIGoogleMap.class.getName()).log(Level.SEVERE, null, ex);
         }
